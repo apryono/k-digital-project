553836584aea4a2e17b86ead178335ee4634aebc
Apryono Boangmanalu <apryonoboang@gmail.com>
2022-02-19 16:49:10 +0700

    Adding read connection database postgresql

--- mini-api/config/config.go
+++ mini-api/config/config.go
@@ -3,8 +3,11 @@ package config
 import (
 	"database/sql"
 	"log"
+	"time"
 
 	"github.com/joho/godotenv"
+	postgrespkg "github.com/k-digital-project/mini-api/pkg/postgresql"
+	"github.com/k-digital-project/mini-api/pkg/str"
 )
 
 //Configs ...
@@ -19,6 +22,25 @@ func LoadConfigs() (res Configs, err error) {
 	if err != nil {
 		log.Fatal("Error loading ..env file")
 	}
+
+	dbConn := postgrespkg.Connection{
+		Host:                    res.EnvConfig["DATABASE_HOST"],
+		DbName:                  res.EnvConfig["DATABASE_DB"],
+		User:                    res.EnvConfig["DATABASE_USER"],
+		Password:                res.EnvConfig["DATABASE_PASSWORD"],
+		Port:                    str.StringToInt(res.EnvConfig["DATABASE_PORT"]),
+		SslMode:                 res.EnvConfig["DATABASE_SSL_MODE"],
+		DBMaxConnection:         str.StringToInt(res.EnvConfig["DATABASE_MAX_CONNECTION"]),
+		DBMAxIdleConnection:     str.StringToInt(res.EnvConfig["DATABASE_MAX_IDLE_CONNECTION"]),
+		DBMaxLifeTimeConnection: str.StringToInt(res.EnvConfig["DATABASE_MAX_LIFETIME_CONNECTION"]),
+	}
+	res.DB, err = dbConn.Connect()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	res.DB.SetMaxOpenConns(dbConn.DBMaxConnection)
+	res.DB.SetMaxIdleConns(dbConn.DBMAxIdleConnection)
+	res.DB.SetConnMaxLifetime(time.Duration(dbConn.DBMaxLifeTimeConnection) * time.Second)
 
 	return res, err
 }
