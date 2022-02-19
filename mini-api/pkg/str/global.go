3386f2681eb3a04b3272f344e09812bc494450dd
Apryono Boangmanalu <apryonoboang@gmail.com>
2022-02-20 05:17:45 +0700

    Adding regex to checking email

--- mini-api/pkg/str/global.go
+++ mini-api/pkg/str/global.go
@@ -1,6 +1,9 @@
 package str
 
-import "strconv"
+import (
+	"regexp"
+	"strconv"
+)
 
 // StringToInt ...
 func StringToInt(data string) int {
@@ -19,4 +22,11 @@ func ShowString(isShow bool, data string) string {
 	}
 
 	return ""
+}
+
+// CheckEmail ...
+func CheckEmail(text string) bool {
+	re := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+	return re.MatchString(text)
 }
