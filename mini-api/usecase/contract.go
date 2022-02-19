a27d870b4530394f1f2bd19243730561bfad649e
Apryono Boangmanalu <apryonoboang@gmail.com>
2022-02-19 22:26:33 +0700

    Adding contract usecase for depedencies

--- mini-api/usecase/contract.go
+++ mini-api/usecase/contract.go
@@ -1,8 +1,15 @@
 package usecase
 
-import "database/sql"
+import (
+	"database/sql"
+
+	ut "github.com/go-playground/universal-translator"
+	validator "github.com/go-playground/validator/v10"
+)
 
 type ContractUC struct {
-	EnvConfig map[string]string
-	DB        *sql.DB
+	EnvConfig  map[string]string
+	DB         *sql.DB
+	Translator ut.Translator
+	Validate   *validator.Validate
 }
