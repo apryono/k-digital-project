42ca508f672755582698fcce4c84911a7c22234b
Apryono Boangmanalu <apryonoboang@gmail.com>
2022-02-19 22:18:08 +0700

    Adding struct bootstrap for initialize depencies

--- mini-api/server/bootstrap/boot.go
+++ mini-api/server/bootstrap/boot.go
@@ -1,6 +1,9 @@
 package bootstrap
 
 import (
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/k-digital-project/mini-api/usecase"
 )
@@ -8,4 +11,6 @@ import (
 type Bootstrap struct {
 	App        *fiber.App
 	ContractUC usecase.ContractUC
+	Validator  *validator.Validate
+	Translator ut.Translator
 }
