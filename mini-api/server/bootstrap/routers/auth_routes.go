424eb28fcf124493fba84844853a339313bbb087
Apryono Boangmanalu <apryonoboang@gmail.com>
2022-02-20 04:30:19 +0700

    Implement middlewares saving context in routes

--- mini-api/server/bootstrap/routers/auth_routes.go
+++ mini-api/server/bootstrap/routers/auth_routes.go
@@ -1,7 +1,11 @@
 package routers
 
 import (
+	"time"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/k-digital-project/mini-api/middlewares"
+	"github.com/k-digital-project/mini-api/pkg/str"
 	"github.com/k-digital-project/mini-api/server/handlers"
 )
 
@@ -16,5 +20,6 @@ func (route AuthRoutes) RegisterRoute() {
 	handler := handlers.AuthHandler{Handler: route.Handler}
 
 	r := route.RouterGroup.Group("/api/auth")
+	r.Use(middlewares.SavingContextValue(time.Duration(str.StringToInt(route.Handler.ContractUC.EnvConfig["APP_TIMEOUT"])) * time.Second))
 	r.Post("/register/email", handler.RegisterByEmail)
 }
