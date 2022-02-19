301e3fde98215c3cff928a3f1584ed52bf78e30e
Apryono Boangmanalu <apryonoboang@gmail.com>
2022-02-19 22:18:38 +0700

    Adding function use to register all routes

--- mini-api/server/bootstrap/routers.go
+++ mini-api/server/bootstrap/routers.go
@@ -4,18 +4,27 @@ import (
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/k-digital-project/mini-api/server/bootstrap/routers"
 	"github.com/k-digital-project/mini-api/server/handlers"
 )
 
 //RegisterRouters regiter all routers
 func (boot Bootstrap) RegisterRouters() {
-	_ = handlers.Handlers{
+	handler := handlers.Handler{
 		FiberApp:   boot.App,
 		ContractUC: &boot.ContractUC,
+		Validator:  boot.Validator,
+		Translator: boot.Translator,
 	}
 
 	boot.App.Get("/", func(ctx *fiber.Ctx) error {
 		return ctx.Status(http.StatusOK).JSON("success")
 	})
+
+	apiV1 := boot.App.Group("/v1")
+
+	// Auth Routes
+	authRoutes := routers.AuthRoutes{RouterGroup: apiV1, Handler: handler}
+	authRoutes.RegisterRoute()
 
 }
